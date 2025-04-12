b39eeae4b05235a40653b38101e398bee013b5f8
Maxim <125602230+BloodyAngel22@users.noreply.github.com>
2025-04-12 20:36:14 +0300

    [DEV-87] Added components for authorization. (#9)
    * [DEV-87] Added components for authorization.
    
    Added components responsible for authorization. Added related hooks,
    functions and storage.  Added provider for token update on all pages.
    Added validation scheme for registration and authorization. Added
    validation scheme for registration and authorization.
    
    * [DEV-87] Removed www and moved next-app to the root of the repository.
    
    * [DEV-87] Moved the next project files to the root of the repository

---
+++ app/types/IRecommendations.ts
@@ -0,0 +1,16 @@
+export interface IRecommendationVideosInfo {
+  success: boolean,
+  status_code: number,
+  data: {
+    page: number,
+    page_size: number,
+    items: {
+      video_uuid: string,
+    } []
+  },
+};
+
+export interface IRecommendationVideoParams {
+  page_size: number,
+  page: number,
+};
